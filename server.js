3da175efc98b8e95f2dd1a99dc38542acda320ae
mellefmedali <mellef_med_ali@hotmail.fr>
2022-12-28 14:16:09 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var app = express();
+var mongoose = require("mongoose")
+
+// activate environment variables
+var dotenv = require('dotenv')
+dotenv.config()
+let port = process.env.PORT
+mongoose.connect(
+    process.env.DATABASE_URL,
+    null
+    ,
+    (err) => {
+        if (err) {
+            console.log('error while connection to db --> : ', err)
+        }else{
+            console.log('connected successfully to the DB')
+        }
+    }
+)
+
+
+app.listen(port, () => console.log('app connected to port'))
